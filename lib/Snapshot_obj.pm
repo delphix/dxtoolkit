b5afd21c0442893fe992bf7b357bbb5693d7884f
Marcin Przepiorowski <marcin@delphix.com>
2019-09-09 19:33:30 +0100

    fixes for timezones not recognized by perl

--- lib/Snapshot_obj.pm
+++ lib/Snapshot_obj.pm
@@ -329,27 +329,58 @@ sub getSnapshotTime {
 # if timezone defined as GMT+/-offset return undef
 # parameters:
 # - timezone
-# Return 0 if OK
+# Return timezone of OK or N/A if timezone is not recognized by perl
 
 sub checkTZ {
     my $self = shift;
     my $timezone = shift;
     logger($self->{_debug}, "Entering Snapshot_obj::checkTZ",1);
 
+    # fixes for timezones supported by Delphix but not recognized by Perl
+    if ($timezone eq 'Etc/Zulu') {
+        $timezone = 'UTC';
+    }
+    if ($timezone eq 'Zulu') {
+        $timezone = 'UTC';
+    }
+    if ($timezone eq 'Etc/Universal') {
+        $timezone = 'UTC';
+    }
+    if ($timezone eq 'Universal') {
+        $timezone = 'UTC';
+    }
+    if ($timezone eq 'Etc/Greenwich') {
+        $timezone = 'GMT';
+    }
+    if ($timezone eq 'Greenwich') {
+        $timezone = 'GMT';
+    }
+    if ($timezone eq 'GMT0') {
+        $timezone = 'GMT';
+    }
+    if ($timezone eq 'Etc/GMT0') {
+        $timezone = 'GMT';
+    }
+    if ($timezone eq 'Etc/GMT-0') {
+        $timezone = 'GMT';
+    }
+    if ($timezone eq 'Etc/GMT+0') {
+        $timezone = 'GMT';
+    }
 
     my $checktime = time();
     my $dt = ParseDate($checktime);
     my $tz = new Date::Manip::TZ;
     my ($err,$date,$offset,$isdst,$abbrev) = $tz->convert_from_gmt($dt, $timezone);
 
-    if (defined($abbrev)) {
+    if (!$err) {
         logger($self->{_debug}, "checkTZ abbrev-" . $abbrev ,1);
     } else {
         logger($self->{_debug}, "checkTZ abbrev-undefined" ,1);
+        $timezone = 'N/A';
     }
 
-    return $err;
-
+    return $timezone;
 }
 
 # Procedure getSnapshotTimeZone
@@ -366,30 +397,11 @@ sub getSnapshotTimeZone {
     chomp($ts);
     my @temp = split(',',$ts);
     my $ret = $temp[0];
-    if ($ret eq 'Etc/Zulu') {
-        $ret = 'Etc/GMT';
-    }
-
-    if ($ret eq 'Greenwich') {
-        $ret = 'GMT+00:00';
-    }
-
-    my $tz = new Date::Manip::TZ;
-
-    my @zone = ('Etc/GMT');
-    my ($err,$val) = $tz->define_offset('+0000', @zone);
-
-    logger($self->{debug}, "Setting GMT timezone err-" . $err );
-
-    @zone = ('Asia/Singapore');
-    ($err,$val) = $tz->define_offset('+0800', @zone);
-
-    logger($self->{debug}, "Setting SGT timezone err-" . $err );
 
     if (! ($ret =~ /[a-zA-Z]{3}.\d\d:\d\d/ )) {
-      if ($self->checkTZ($ret)) {
-        $ret = 'N/A';
-      }
+      # if timezone is not GMT+-00:00 format
+      # check if this format can be recognized and amended by checkTZ function
+      $ret = $self->checkTZ($ret);
     }
 
     return $ret;
