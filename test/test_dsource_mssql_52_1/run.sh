8935abca4c6a642f942c2f8b9bccc478e4d20ffe
Marcin Przepiorowski <marcin@delphix.com>
2020-01-09 15:43:53 +0000

    test cleanup

--- test/test_dsource_mssql_52_1/run.sh
+++
@@ -1,8 +0,0 @@
-pioro-mbp:bin mprzepiorowski$ perl dx_ctl_dsource.pl -d local -action create -group "Sources" -creategroup -dsourcename "tpcc"  -type mssql -sourcename "tpcc" -sourceinst "KVMTARGET2012" -sourceenv "WIN2012N1STD" -source_os_user "DELPHIX\delphix_admin" -dbuser delphixdb -password delphixdb -logsync yes -stageinst "KVMTARGET2012" -stageenv "WIN2012N1STD" -stage_os_user "DELPHIX\delphix_admin" -validatedsync TRANSACTION_LOG -backup_dir ""
-Waiting for all actions to complete. Parent action is ACTION-273180
-Action completed with success
-pioro-mbp:bin mprzepiorowski$
-pioro-mbp:bin mprzepiorowski$
-pioro-mbp:bin mprzepiorowski$ perl dx_ctl_dsource.pl -d local -action create -group "Sources" -creategroup -dsourcename "simple"  -type mssql -sourcename "simple" -sourceinst "KVMTARGET2012" -sourceenv "WIN2012N1STD" -source_os_user "DELPHIX\delphix_admin" -dbuser delphixdb -password delphixdb -logsync no -stageinst "KVMTARGET2012" -stageenv "WIN2012N1STD" -stage_os_user "DELPHIX\delphix_admin" -delphixmanaged yes
-Waiting for all actions to complete. Parent action is ACTION-273200
-Action completed with success
--- test/test_hierarchy/start_server.pl
+++
@@ -1,6 +0,0 @@
-use server;
-
-my $server = server->new(8080);
-$server->set_dir('Delphix32');
-$server->host('127.0.0.1');
-$server->run();
