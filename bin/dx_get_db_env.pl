1299973230b67da6d38f7e6ce72064b824cea5b6
Marcin Przepiorowski <marcin@delphix.com>
2021-10-08 11:26:30 +0100

    snapshot fix - parameter added

--- bin/dx_get_db_env.pl
+++ bin/dx_get_db_env.pl
@@ -85,6 +85,7 @@ GetOptions(
   'all' => (\my $all),
   'version' => \(my $print_version),
   'nohead' => \(my $nohead),
+  'snappervdb' => \(my $snappervdb),
   'configfile|c=s' => \(my $config_file)
 ) or pod2usage(-verbose => 1,  -input=>\*DATA);
 
@@ -410,10 +411,14 @@ for my $engine ( sort (@{$engine_list}) ) {
       if (lc $parentlast eq 'p') {
         if (($parentsnap ne '') && ($dbobj->getType() eq 'VDB')) {
           if (defined($snapshots)) {
-            if ($snapshots->getSnapshotPerRef($parentsnap) == 1) {
-              print "Problem with loading snapshot\n";
-              $ret = $ret + 1;
-              next
+            if (defined($snappervdb)) {
+              if ($snapshots->getSnapshotPerRef($parentsnap) == 1) {
+                print "Problem with loading snapshot\n";
+                $ret = $ret + 1;
+                next
+              }
+            } else {
+              $snapshots->getSnapshotList();
             }
           } else {
             print "Snapshot object not created\n";
@@ -662,6 +667,12 @@ p - parent snapshot for VDB
 Change a hostname/env column to display :
 h - target host name (default)
 e - target environment name
+
+=item B<-snappervdb>
+By default snapshots for all objects are read to avoid an API call per VDB.
+This behaviour is not efficient for engine with many snapshots and this parameter
+is switching logic to read one snapshot per VDB. It should be used for Engines
+with many (1000's) snapshots
 
 
 =item B<-format>
